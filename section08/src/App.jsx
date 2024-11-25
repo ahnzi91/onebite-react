1e3dd858f35733c4c326b2dc19c8191ef9610f2a
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-11-25 16:33:33 +0900

    8.4) Create - Todo 추가하기

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,13 +1,57 @@
+import { useState, useRef } from "react";
+
 import "./App.css";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
 
+// Mock Data
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
+
+  // Mock Data와 겹치지 않게 id 값 3으로 초기화
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    // todos와 같은 상태의 값은 반드시 상태 변화 함수를 호출해서  수정해야 한다.
+    // 그래야만 변경된 state의 값을 React가 감지하고 컴포넌트를 정상적으로 리렌더링 할 수 있기 때문이다.
+    // push() 메서드를 이용해서 state 값을 직접 변경하면 안되고, 무조건 상태 변화 함수인 setTodo를 호출하고 인수로 새로운 값을 넣어줘야 한다.
+    // spread 연산자를 이용해서 기존의 todos 배열에 들어있던 값들을 펼쳐주어야 한다.
+    setTodos([newTodo, ...todos]);
+  };
+
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
--- section08/src/components/Editor.jsx
+++ section08/src/components/Editor.jsx
@@ -1,10 +1,42 @@
+import { useState, useRef } from "react";
+
 import "./Editor.css";
 
-const Editor = () => {
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  // Enter 키 사용 시 등록하게 해주는 이벤트 핸들러
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    // 빈 Todo 방지하기
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+
   return (
     <div className="Editor">
-      <input placeholder="새로운 Todo..." />
-      <button>추가</button>
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeyDown}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
     </div>
   );
 };
