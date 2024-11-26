ef8c987759adb7484dc9101771f70d482d04bbaf
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-11-26 20:35:27 +0900

    9.2) 투두리스트 업그레이드

--- section09/src/App.jsx
+++ section09/src/App.jsx
@@ -1,10 +1,9 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useReducer } from "react";
 
 import "./App.css";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
-import Exam from "./components/Exam";
 
 // Mock Data
 const mockData = [
@@ -28,59 +27,56 @@ const mockData = [
   },
 ];
 
-function App() {
-  const [todos, setTodos] = useState(mockData);
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        item.id === action.targetId ? { ...item, isDone: !item.isDone } : item
+      );
+    case "DELETE":
+      return state.filter((item) => item.id !== action.targetId);
+    default:
+      return state;
+  }
+}
 
-  // Mock Data와 겹치지 않게 id 값 3으로 초기화
+function App() {
+  const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
   const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    };
-
-    // todos와 같은 상태의 값은 반드시 상태 변화 함수를 호출해서  수정해야 한다.
-    // 그래야만 변경된 state의 값을 React가 감지하고 컴포넌트를 정상적으로 리렌더링 할 수 있기 때문이다.
-    // push() 메서드를 이용해서 state 값을 직접 변경하면 안되고, 무조건 상태 변화 함수인 setTodo를 호출하고 인수로 새로운 값을 넣어줘야 한다.
-    // spread 연산자를 이용해서 기존의 todos 배열에 들어있던 값들을 펼쳐주어야 한다.
-    setTodos([newTodo, ...todos]);
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
   };
 
-  // 체크 박스를 위한 함수
   const onUpdate = (targetId) => {
-    // todos State의 값들 중에 targetId와 일치하는 id를 갖는 todo 아이템의 isDone 변경
-
-    // 인수 : todos 배열에서 targetId와 일치하는 id를 갖는 요소의 데이터만 바꾼 새로운 배열
-    setTodos(
-      // todos.map((todo) => {
-      //   if (todo.id === targetId) {
-      //     return {
-      //       ...todo,
-      //       isDone: !todo.isDone,
-      //     };
-      //   }
-      //   return todo;
-      // })
-
-      // 삼항 연산자로 간단하게 표현
-      todos.map((todo) => (todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo))
-    );
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId,
+    });
   };
 
   const onDelete = (targetId) => {
-    // 인수 : todos 배열에서 targetId와 일치하는 id를 갖는 요소만 삭제한 새로운 배열
-    setTodos(todos.filter((todo) => todo.id !== targetId));
+    dispatch({
+      type: "DELETE",
+      targetId: targetId,
+    });
   };
 
   return (
     <div className="App">
-      <Exam />
-      {/* <Header />
+      <Header />
       <Editor onCreate={onCreate} />
-      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} /> */}
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
     </div>
   );
 }
