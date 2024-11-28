bf436656a42ed3a0769a7f4ec910ea95e416a52e
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-11-28 22:04:45 +0900

    10.3) React.memo와 컴포넌트 렌더링 최적화

--- section10/src/components/TodoItem.jsx
+++ section10/src/components/TodoItem.jsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import "./TodoItem.css";
 
 const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
@@ -19,4 +20,16 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   );
 };
 
-export default TodoItem;
+export default memo(TodoItem, (prevProps, nextProps) => {
+  // 반환값에 따라 Props가 바뀌었는지 안바뀌었는지 판단
+  // true : Props 바뀌지 않음 -> 리렌더링 X
+  // false : Props 바뀜 -> 리렌더링 O
+
+  if (prevProps.id !== nextProps.id) return false;
+  if (prevProps.isDone !== nextProps.isDone) return false;
+  if (prevProps.content !== nextProps.content) return false;
+  if (prevProps.date !== nextProps.date) return false;
+
+  // id, isDone, content, date가 바뀌지 않았다면 true 리턴
+  return true;
+});
