b1b2f963b8d8153e13e392953efb75eb222bbe1a
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-11-30 16:06:45 +0900

    11.2) Context 사용하기

---
+++ section11/src/components/Editor.jsx
@@ -0,0 +1,47 @@
+import { useState, useContext, useRef } from "react";
+import { TodoContext } from "../App";
+
+import "./Editor.css";
+
+const Editor = () => {
+  const { onCreate } = useContext(TodoContext);
+
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  // Enter 키 사용 시 등록하게 해주는 이벤트 핸들러
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    // 빈 Todo 방지하기
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+
+  return (
+    <div className="Editor">
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeyDown}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
+};
+
+export default Editor;
---
+++ section11/src/components/TodoItem.jsx
@@ -0,0 +1,40 @@
+import { memo, useContext } from "react";
+import { TodoContext } from "../App";
+import "./TodoItem.css";
+
+const TodoItem = ({ id, content, isDone, date }) => {
+  const { onUpdate, onDelete } = useContext(TodoContext);
+
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  };
+
+  const onClickDeleteButton = () => {
+    onDelete(id);
+  };
+
+  return (
+    <div className="TodoItem">
+      <input type="checkbox" checked={isDone} onChange={onChangeCheckbox} />
+      <div className="content">{content}</div>
+      <div className="date">{new Date(date).toLocaleDateString()}</div>
+      <button onClick={onClickDeleteButton}>삭제</button>
+    </div>
+  );
+};
+
+// export default memo(TodoItem, (prevProps, nextProps) => {
+// 반환값에 따라 Props가 바뀌었는지 안바뀌었는지 판단
+// true : Props 바뀌지 않음 -> 리렌더링 X
+// false : Props 바뀜 -> 리렌더링 O
+
+// if (prevProps.id !== nextProps.id) return false;
+// if (prevProps.isDone !== nextProps.isDone) return false;
+// if (prevProps.content !== nextProps.content) return false;
+// if (prevProps.date !== nextProps.date) return false;
+
+// id, isDone, content, date가 바뀌지 않았다면 true 리턴
+// return true;
+// });
+
+export default memo(TodoItem);
