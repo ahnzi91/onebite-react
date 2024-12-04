f3e3c794d23e51e9120f3f7c8e1640e237245eef
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-12-04 13:22:33 +0900

    12.4) 페이지 라우팅 3. 페이지 이동

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 
 import "./App.css";
 
@@ -11,13 +11,28 @@ import NotFound from "./pages/NotFound";
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
+  // useNavigate() : 페이지를 실제로 이동시키는 navigate() 함수를 반환해줍니다.
+  const navigate = useNavigate();
+
+  const onClickButton = () => {
+    navigate("/new");
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new" element={<New />} />
-      <Route path="/diary" element={<Diary />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary </Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
