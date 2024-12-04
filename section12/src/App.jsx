59ff0e059fb2531ace754913e811aa94b18416e4
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-12-04 17:29:03 +0900

    12.7) 공통 컴포넌트 구현하기

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -8,33 +8,41 @@ import Diary from "./pages/Diary";
 import New from "./pages/New";
 import NotFound from "./pages/NotFound";
 
+import Header from "./components/Header";
+import Button from "./components/Button";
+
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
-  // useNavigate() : 페이지를 실제로 이동시키는 navigate() 함수를 반환해줍니다.
-  const navigate = useNavigate();
-
-  const onClickButton = () => {
-    navigate("/new");
-  };
-
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
+      <Header
+        title={"Header"}
+        leftChild={<Button text={"Left"} />}
+        rightChild={<Button text={"Right"} />}
+      />
 
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary </Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Button
+        text={"123"}
+        onClick={() => {
+          console.log("123 버튼 클릭");
+        }}
+      />
+      <Button
+        type={"POSITIVE"}
+        text={"123"}
+        onClick={() => {
+          console.log("123 버튼 클릭");
+        }}
+      />
+      <Button
+        type={"NEGATIVE"}
+        text={"123"}
+        onClick={() => {
+          console.log("123 버튼 클릭");
+        }}
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
