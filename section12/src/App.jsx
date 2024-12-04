46261f42da992ba1f2051066e1215358f69d85e2
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-12-04 16:41:15 +0900

    12.6) 폰트, 이미지, 레이아웃 설정하기

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 
 import "./App.css";
+import { getEmotionImage } from "./util/get-emotion-image";
 
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
@@ -20,6 +21,14 @@ function App() {
 
   return (
     <>
+      <div>
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
+
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
