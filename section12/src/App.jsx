334fce82ea6a03943bddb488240f6183c1192add
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-12-09 23:01:05 +0900

    12.11)Home 페이지 구현하기 2. 기능

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -12,15 +12,21 @@ import NotFound from "./pages/NotFound";
 const mockData = [
   {
     id: 1,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2024-12-09").getTime(),
     emotionId: 1,
     content: "1번 일기 내용",
   },
   {
     id: 2,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2024-12-08").getTime(),
     emotionId: 2,
     content: "2번 일기 내용",
+  },
+  {
+    id: 3,
+    createdDate: new Date("2024-11-11").getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용",
   },
 ];
 
@@ -39,8 +45,8 @@ function reducer(state, action) {
 }
 
 // 일기 데이터를 공급할 Context
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,16 +1,51 @@
+import { useState, useContext } from "react";
+
+import { DiaryStateContext } from "../App";
+
 import Header from "../components/Header";
 import Button from "../components/Button";
 import DiaryList from "../components/DiaryList";
 
+const getMonthlyData = (pivotDate, data) => {
+  // 이번 달 시작 시간과 끝 시간을 알아야 한다.
+  // 1, 0, 0, 0 : 1일 0시 0분 0초
+  const beginTime = new Date(pivotDate.getFullYear(), pivotDate.getMonth(), 1, 0, 0, 0).getTime();
+
+  // getMonth() + 1, 0 : 예를 들어 2월의 0일 이라고 하면 1월의 마지막 날이 된다.
+  const endTime = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth() + 1,
+    0,
+    23,
+    59,
+    59
+  ).getTime();
+
+  return data.filter((item) => beginTime <= item.createdDate && item.createdDate <= endTime);
+};
+
 const Home = () => {
+  const data = useContext(DiaryStateContext);
+
+  const [pivotDate, setPivotDate] = useState(new Date());
+
+  const monthlyData = getMonthlyData(pivotDate, data);
+
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
+
   return (
     <div>
       <Header
-        title={"2024년 12월"}
-        leftChild={<Button text={"<"} />}
-        rightChild={<Button text={">"} />}
+        title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+        leftChild={<Button text={"<"} onClick={onDecreaseMonth} />}
+        rightChild={<Button text={">"} onClick={onIncreaseMonth} />}
       />
-      <DiaryList />
+      <DiaryList data={monthlyData} />
     </div>
   );
 };
