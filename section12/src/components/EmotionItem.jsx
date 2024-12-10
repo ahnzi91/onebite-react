77e19e15328dddbb7154f2133b16b1cb72e610dc
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-12-10 15:28:03 +0900

    12.14)New 페이지 구현하기 2. 기능

--- section12/src/components/EmotionItem.jsx
+++ section12/src/components/EmotionItem.jsx
@@ -2,9 +2,12 @@ import { getEmotionImage } from "../util/get-emotion-image";
 
 import "./EmotionItem.css";
 
-const EmotionItem = ({ emotionId, emotionName, isSelected }) => {
+const EmotionItem = ({ emotionId, emotionName, isSelected, onClick }) => {
   return (
-    <div className={`EmotionItem ${isSelected ? `EmotionItem_on_${emotionId}` : ""}`}>
+    <div
+      className={`EmotionItem ${isSelected ? `EmotionItem_on_${emotionId}` : ""}`}
+      onClick={onClick}
+    >
       <img className="emotion_img" src={getEmotionImage(emotionId)} />
       <div className="emotion_name">{emotionName}</div>
     </div>
--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,12 +1,28 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { DiaryDispatchContext } from "../App";
+
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
 
 const New = () => {
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const nav = useNavigate();
+
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    nav("/", { replace: true });
+  };
+
   return (
     <div>
-      <Header title={"새 일기 쓰기"} leftChild={<Button text={"<뒤로 가기"} />} />
-      <Editor />
+      <Header
+        title={"새 일기 쓰기"}
+        leftChild={<Button text={"< 뒤로 가기"} onClick={() => nav(-1)} />}
+      />
+      <Editor onSubmit={onSubmit} />
     </div>
   );
 };
